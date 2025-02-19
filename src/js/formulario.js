d62de28f91994394d4fa28c490df741d38bdf28e
João Víctor de Araujo Lima <joaovictor.ne@gmail.com>
2025-02-19 10:21:43 -0300

    Update js with less code

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -3,38 +3,18 @@ const campoEmail = document.getElementsByName("email")[0];
 const campoTelefone = document.getElementsByName("telefone")[0];
 const campoMensagem = document.getElementsByName("mensagem")[0];
 
+const campos = [campoNome, campoEmail, campoTelefone, campoMensagem];
+
 const campoInvalidoTexto = document.getElementsByClassName("invalido");
 
-campoNome.addEventListener("change", () => {
-  campoNome.classList.add("campo-valido");
-  if (campoNome.classList.contains("campo-invalido")) {
-    campoNome.classList.remove("campo-invalido");
-    campoInvalidoTexto[0].classList.remove("visivel");
-  }
-});
-
-campoEmail.addEventListener("change", () => {
-  campoEmail.classList.add("campo-valido");
-  if (campoEmail.classList.contains("campo-invalido")) {
-    campoEmail.classList.remove("campo-invalido");
-    campoInvalidoTexto[1].classList.remove("visivel");
-  }
-});
-
-campoTelefone.addEventListener("change", () => {
-  campoTelefone.classList.add("campo-valido");
-  if (campoTelefone.classList.contains("campo-invalido")) {
-    campoTelefone.classList.remove("campo-invalido");
-    campoInvalidoTexto[2].classList.remove("visivel");
-  }
-});
-
-campoMensagem.addEventListener("change", () => {
-  campoMensagem.classList.add("campo-valido");
-  if (campoMensagem.classList.contains("campo-invalido")) {
-    campoMensagem.classList.remove("campo-invalido");
-    campoInvalidoTexto[3].classList.remove("visivel");
-  }
+campos.forEach((item, indice) => {
+  item.addEventListener("change", () => {
+    item.classList.add("campo-valido");
+    if (item.classList.contains("campo-invalido")) {
+      item.classList.remove("campo-invalido");
+      campoInvalidoTexto[indice].classList.remove("visivel");
+    }
+  });
 });
 
 const formulario = document.getElementsByClassName("formulario")[0];
@@ -46,30 +26,15 @@ formulario.addEventListener("submit", (event) => {
 });
 
 function verificarCampos() {
-  if (campoNome.value == "") {
-    campoNome.classList.add("campo-invalido");
-    campoInvalidoTexto[0].classList.add("visivel");
-  }
-  if (campoEmail.value == "") {
-    campoEmail.classList.add("campo-invalido");
-    campoInvalidoTexto[1].classList.add("visivel");
-  }
-  if (campoTelefone.value == "") {
-    campoTelefone.classList.add("campo-invalido");
-    campoInvalidoTexto[2].classList.add("visivel");
-  }
-  if (campoMensagem.value == "") {
-    campoMensagem.classList.add("campo-invalido");
-    campoInvalidoTexto[3].classList.add("visivel");
-  }
-  if (
-    campoNome.value == "" ||
-    campoEmail.value == "" ||
-    campoTelefone.value == "" ||
-    campoMensagem.value == ""
-  ) {
-    return false;
-  } else {
-    return true;
-  }
+  let validacao = true;
+
+  campos.forEach((item, indice) => {
+    if (item.value == "") {
+      item.classList.add("campo-invalido");
+      campoInvalidoTexto[indice].classList.add("visivel");
+      validacao = false;
+    }
+  });
+
+  return validacao;
 }
